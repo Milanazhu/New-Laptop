ae2e27f3ed4fa88c9a09419fc3ffadc33719883c
Milana Zhusubalieva <milano4ka@172.24.25.221-spb.dhcp.yndx.net>
2024-12-06 20:12:01 +0300

    Поиск по ДОМ элементу

--- 7_3Поиск_DOM_элементов/7_3.js
+++ 7_3Поиск_DOM_элементов/7_3.js
@@ -1,4 +1,4 @@
-function filter(arr, search) {
+function find(arr, search) {
     let result = -1;
 
     for (let i = 0; i < arr.length; i++) {
@@ -11,13 +11,31 @@ function filter(arr, search) {
 }
 
 const product = ['apple', 'banana', 'orange', 'pear',];
+function listrender(arr) {
+    const listEl = document.createElement('ul');
 
-const listEl = document.createElement('ul');
-
-for(let i = 0; i < product.length; i++) {
-    const liEl = document.createElement("li") // создаем новый элемент li
-    liEl.textContent = `${i}) ${product[i]}` // добавляем текст в li и с помощью знака § передаем индекс в массиве  `${i}` 
-    listEl.append(liEl); // добавляем li в ul
+    for(let i = 0; i < arr.length; i++) {
+        const liEl = document.createElement("li") // создаем новый элемент li
+        liEl.textContent = `${i+1}) ${arr[i]}` // добавляем текст в li и с помощью знака § передаем индекс в массиве  `${i}` 
+        listEl.append(liEl); // тут я добавила liEL в listEl в конец, а создала я этот listEl элемент в 15 строке 
+    }
+    document.body.append(listEl);
 }
 
-document.body.append(listEl);
+const buttonSearch = document.createElement("button"); 
+buttonSearch.textContent = "button";
+document.body.append(buttonSearch);
+
+buttonSearch.onclick = function () {
+    const search = prompt("Enter the product name:");
+    const findIndex = find(product,search)
+    if(findIndex > -1) {
+        document.querySelector(`li:nth-child(${findIndex + 1})`).style.color = "red";
+    } else {
+        alert("Product not found")
+    }
+}
+listrender(product);
+
+
+
